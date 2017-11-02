79505bb92ca2826f1ebaf029cba9657a10511380
frankwolffnl <frankwolff.nl@gmail.com>
2017-11-02 11:08:40 +0100

    Image view implemented.

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -35,12 +35,17 @@ class ViewController: UITableViewController {
 		cell.textLabel?.text = pictures[indexPath.row]
 		return cell
 	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+			vc.selectedImage = pictures[indexPath.row]
+			navigationController?.pushViewController(vc, animated: true)
+		}
+	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
 }
 
